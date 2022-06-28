0a2de9f3652844f644e607f13cd11a516c0bc77b
Chloe <spronkoid@gmail.com>
2022-06-28 09:28:34 -0500

    Update tokenizer.py

--- datasettokenizer/tokenizer.py
+++ datasettokenizer/tokenizer.py
@@ -1,19 +1,22 @@
-block_size = 128
+_block_size = 128
 
 def group_texts(examples):
-    global block_size
+    global _block_size
     concatenated_examples = {k: sum(examples[k], []) for k in examples.keys()}
     total_length = len(concatenated_examples[list(examples.keys())[0]])
-    total_length = (total_length // block_size) * block_size
+    total_length = (total_length // _block_size) * _block_size
     result = {
-        k: [t[i : i + block_size] for i in range(0, total_length, block_size)]
+        k: [t[i : i + _block_size] for i in range(0, total_length, _block_size)]
         for k, t in concatenated_examples.items()
     }
     result["labels"] = result["input_ids"].copy()
     return result
 
 def tokenize_dataset(lm_datasets, tokenizer, block_size):
-    global block_size
+    # sketchy ass code
+    global _block_size
+    _block_size = block_size
+    
     tokenize_function = lambda examples: tokenizer(examples["text"])
     tokenized_datasets = lm_datasets.map(tokenize_function, batched=True, num_proc=4, remove_columns=["text"])
     lm_datasets = tokenized_datasets.map(
